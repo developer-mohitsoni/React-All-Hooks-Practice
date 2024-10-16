9fd8e4ce932acff19f6f50848bf4c6f886d8b3c4
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-16 16:11:30 +0530

    useRefHook - Example-2

--- src/components/UseRefHook.jsx
+++ src/components/UseRefHook.jsx
@@ -1,18 +1,29 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const UseRefHook = () => {
   const countValue = useRef(0);
+  const divReference = useRef();
 
   const handleClick = () => {
     countValue.current++;
 
     console.log(countValue.current);
   };
+
+  useEffect(() => {
+    const getDivReference = divReference.current;
+
+    console.log(getDivReference);
+  }, []);
+
   return (
     <>
       <div>
         <h1>Use Ref Hook</h1>
         <button onClick={handleClick}>Click Me</button>
+        <div ref={divReference} className="hello">
+          Hello World
+        </div>
       </div>
     </>
   );
