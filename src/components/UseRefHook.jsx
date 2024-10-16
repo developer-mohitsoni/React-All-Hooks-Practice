98843becdaae60d2167bae0e1e04ac5d25d7b097
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-16 16:27:42 +0530

    useRefHook - Example-3

--- src/components/UseRefHook.jsx
+++ src/components/UseRefHook.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 const UseRefHook = () => {
   const countValue = useRef(0);
   const divReference = useRef();
+  const inputReference = useRef();
 
   const handleClick = () => {
     countValue.current++;
@@ -16,6 +17,15 @@ const UseRefHook = () => {
     console.log(getDivReference);
   }, []);
 
+  // On Page load it will focus on this input
+  useEffect(() => {
+    inputReference.current.focus();
+    console.log(inputReference.current);
+  }, []);
+
+  // This below input is not yet created in the dom tree that's why it is undefined.
+  console.log(inputReference.current);
+
   return (
     <>
       <div>
@@ -24,6 +34,11 @@ const UseRefHook = () => {
         <div ref={divReference} className="hello">
           Hello World
         </div>
+        <input
+          ref={inputReference}
+          type="text"
+          placeholder="Enter Some Points..."
+        />
       </div>
     </>
   );
